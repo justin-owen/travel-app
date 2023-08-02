1d3826d91cc8723cf44a3aab3a03374472153e84
justin-owen <justinowen22345@gmail.com>
2023-08-02 06:14:50 -0400

    database connected

--- routes/home/main.js
+++ routes/home/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
-
+const user = require('../../models/user')
+const bodyParser = require('body-parser')
 router.get('/', (req, res)=>{
     res.render('home/index')
 });
@@ -11,5 +11,21 @@ router.get('/regions', (req, res)=>{
 router.get('/countries', (req, res)=>{
     res.render('home/countries')
 });
+router.post('/', (req, res) =>{
+    try{
+        const newUser = new user({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        newUser.save()
+        console.log(newUser)
+        res.render('home/index')
+    }catch (error){
+        console.log(error)
+    }
 
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, family: 4}).then((db)=>{
+    console.log('MONGO connected');
+}).catch(err => console.log(err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.json())
 
@@ -11,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 const main = require('./routes/home/main')
 
 app.use('/', main)
+
+app.post('/')
 
 app.listen(4000, () => console.log(`server running on 4000`))
 
