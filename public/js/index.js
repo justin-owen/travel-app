1c86a74097948b94ce31aaf1749aa91e11a92c2b
justin-owen <justinowen22345@gmail.com>
2023-08-02 00:44:12 -0400

    updated home page

--- public/js/index.js
+++ public/js/index.js
@@ -7,17 +7,19 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link')
 const registerLink = document.querySelector('.register-link')
 const iconclose = document.querySelector('.icon-close')
+
+//carousel of popular regions
 var sectionIndex = 0;
 leftArrow.addEventListener('click', function(){
-    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 3;
     slider.style.transform = 'translate(' + (sectionIndex) * -25 + `%)`
 });
 
 rightArrow.addEventListener('click', function(){
-    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
+    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 0;
     slider.style.transform = 'translate(' + (sectionIndex) * -25 + `%)`
 });
-
+// pop up login and registration
 loginNav.addEventListener('click', function(){
     wrapper.classList.remove('active-popup')
     wrapper.classList.add('login')
